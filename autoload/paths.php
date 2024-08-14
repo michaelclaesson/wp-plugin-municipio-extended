dc72918ccc78d140cb3cff22f2384354c7ea5d98
Fredrik Johansson <jeanfredrik@gmail.com>
2024-08-14 22:03:40 +0200

    Remove custom mu-plugins from paths

--- autoload/paths.php
+++ autoload/paths.php
@@ -4,7 +4,7 @@ add_filter(
   "Modularity/Module/TemplatePath",
   function ($paths) {
     $new_paths[] = MUNICIPIO_EXTENDED_PATH . "/views";
-    $new_paths[] = MU_PLUGINS_DIR . "/views";
+    // $new_paths[] = MU_PLUGINS_DIR . "/views";
     $paths = array_merge($paths, $new_paths);
     return $paths;
   },
@@ -13,14 +13,14 @@ add_filter(
 
 add_filter("Modularity/CoreTemplatesSearchPaths", function ($paths) {
   $new_paths[] = MUNICIPIO_EXTENDED_PATH . "/views";
-  $new_paths[] = MU_PLUGINS_DIR . "/views";
+  // $new_paths[] = MU_PLUGINS_DIR . "/views";
   $paths = array_merge($paths, $new_paths);
   return $paths;
 });
 
 add_filter("Municipio/viewPaths", function ($paths) {
   $new_paths[] = MUNICIPIO_EXTENDED_PATH . "/views";
-  $new_paths[] = MU_PLUGINS_DIR . "/views";
+  // $new_paths[] = MU_PLUGINS_DIR . "/views";
   $paths = array_merge($paths, $new_paths);
   return $paths;
 });
@@ -29,7 +29,7 @@ add_filter(
   "Municipio/controllerPaths",
   function ($paths) {
     $new_paths[] = MUNICIPIO_EXTENDED_PATH . "/psr-4/Controller";
-    $new_paths[] = MU_PLUGINS_DIR . "/psr-4/Controller";
+    // $new_paths[] = MU_PLUGINS_DIR . "/psr-4/Controller";
     $paths = array_merge($new_paths, $paths);
     return $paths;
   },
@@ -38,7 +38,7 @@ add_filter(
 
 add_filter("helsingborg-stad/blade/controllerPaths", function ($paths) {
   $new_paths[] = MUNICIPIO_EXTENDED_PATH . "/psr-4/ComponentLibrary/Component";
-  $new_paths[] = MU_PLUGINS_DIR . "/psr-4/ComponentLibrary/Component";
+  // $new_paths[] = MU_PLUGINS_DIR . "/psr-4/ComponentLibrary/Component";
   $paths = array_merge($new_paths, $paths);
   return $paths;
 });
