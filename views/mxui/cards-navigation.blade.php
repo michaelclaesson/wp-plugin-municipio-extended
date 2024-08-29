a471928c56dad894be2194544eceb0a49bcc3844
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-08-29 14:05:02 +0200

    feat:adds cards format to mod-navigation

---
+++ views/mxui/cards-navigation.blade.php
@@ -0,0 +1,40 @@
+<ul class="grid grid-cols-[repeat(auto-fill,minmax(min(100%,16rem),1fr))] gap-[--grid-gap] space-y-0">
+    @foreach($items as $item)
+        @php
+        $content = wp_trim_words($item['post']->post_content ?? '', 8, '...');
+        @endphp
+        <li class="lg:max-h-[310px]">
+            @card([
+                'link' => $item['href'],
+                'imageFirst' => true,
+                'heading' => $item['title'],
+                'content' => $content,
+                'tags' => $item['termsUnlinked'] ?? '',
+                'meta' => $item['readingTime'] ?? '',
+                'date' => $item['postDateFormatted'] ?? '',
+                'dateBadge' => $item['dateBadge'] ?? '',
+                'classList' => ['u-height--100', 'cards-navigation'],
+                'containerAware' => true,
+                'hasAction' => true,
+                'hasPlaceholder' => $item['hasPlaceholderImage'] ?? false,
+                'image' => $item['image'] ?? '',
+                'postId' => $item['id'] ?? '',
+                'postType' => $item['postType'] ?? '',
+                'attributeList' => $item['attributeList'] ?? [],
+            ])
+            @includeWhen(!empty($post->callToActionItems['floating']), 'partials.floating')
+            @endcard
+        </li>
+    @endforeach
+</ul>
+
+
+
+
+
+
+
+
+
+
+
