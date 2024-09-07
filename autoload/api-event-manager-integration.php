117909232f65263c39928ba48a0261ccffe7cd21
Fredrik Johansson <jeanfredrik@gmail.com>
2024-09-07 15:53:24 +0200

    Add customizer option to disable event hero overlay

---
+++ autoload/api-event-manager-integration.php
@@ -0,0 +1,24 @@
+<?php
+use Kirki;
+use Municipio\Customizer;
+
+// add_filter("EventManagerIntegration/DisableEventHero", "__return_true");
+add_filter("EventManagerIntegration/DisableEventHeroOverlay", function () {
+  return Kirki::get_option(
+    Customizer::KIRKI_CONFIG,
+    "disable_event_hero_overlay",
+  );
+});
+
+add_action("init", function () {
+  $post_type = "event";
+  $section_id = "municipio_customizer_panel_content_types_{$post_type}";
+  Kirki::add_field(Customizer::KIRKI_CONFIG, [
+    "section" => $section_id,
+    "type" => "checkbox",
+    "settings" => "disable_event_hero_overlay",
+    "label" => __("Disable event hero overlay", "municipio-extended"),
+    "default" => false,
+    "priority" => 20,
+  ]);
+});
