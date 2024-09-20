de76925ca2d9edf930a03d671144d9eba4e6806e
Fredrik Johansson <jeanfredrik@gmail.com>
2024-09-20 17:11:26 +0200

    Remove extra space below main content

--- autoload/municipio-config.php
+++ autoload/municipio-config.php
@@ -1,3 +1,8 @@
 <?php
 
 add_filter("municipio/remove_script_versions", "__return_false");
+
+add_filter("Municipio/Template/viewData", function ($data) {
+  $data["mainContentBottomMargin"] = 0;
+  return $data;
+});
