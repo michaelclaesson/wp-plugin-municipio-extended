7ccebac5b5c48d2de9ea21fe7e6418e52153c5ef
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-10-16 16:36:23 +0200

    fix: deletes option to pick cards color

--- psr-4/Modularity/ModNavigation/ModNavigation.php
+++ psr-4/Modularity/ModNavigation/ModNavigation.php
@@ -114,23 +114,6 @@ class ModNavigation extends MxModule {
           ),
         ],
       ],
-      [
-        "type" => "color",
-        "settings" => "mod_navigation_cards_color",
-        "label" => _x(
-          "Color for format “cards”",
-          "Navigation Module Customization Field Label",
-          "municipio-extended",
-        ),
-        "default" => "#0008DC",
-        "priority" => 10,
-        "output" => [
-          [
-            "element" => ":root",
-            "property" => "--navigation-card-color",
-          ],
-        ],
-      ],
     ];
   }
 
