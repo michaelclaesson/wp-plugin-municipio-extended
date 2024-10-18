7a35782ecc74a08a3d7747121cf83da85bde3571
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-10-18 17:20:46 +0200

    feat: add buttons to navigate for children pages

---
+++ autoload/button-navigation.php
@@ -0,0 +1,50 @@
+<?php
+
+add_filter(
+  "Municipio/Customizer/Sections/General/secondary_navigation_position",
+  function ($options) {
+    $options["choices"]["below_title"] = _x(
+      "Below title",
+      "Secondary navigation position",
+      "municipio-extended",
+    );
+
+    return $options;
+  },
+);
+
+add_action("article_content_before", function () {
+  $post = mx_get_post();
+
+  $args = [
+    "post_parent" => $post->ID,
+    "post_type" => $post->post_type,
+    "nopaging" => true,
+    "post_status" => "publish",
+    "orderby" => "menu_order",
+    "order" => "ASC",
+    "meta_query" => [
+      [
+        "key" => "hide_in_menu",
+        "value" => "1",
+        "compare" => "!=",
+      ],
+    ],
+  ];
+
+  $child_posts = get_posts($args);
+  $items = array_map(function ($post) {
+    return [
+      "title" => get_the_title($post->ID),
+      "href" => get_permalink($post->ID),
+    ];
+  }, $child_posts);
+
+  if (!empty($items)) {
+    echo '<div class="tailwind">';
+    echo mx_render_view("mxui.navigation.buttons", [
+      "items" => $items,
+    ]);
+    echo "</div>";
+  }
+});
