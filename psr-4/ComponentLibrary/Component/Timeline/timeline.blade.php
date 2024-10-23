9a2eccc3bbc8a295ebd957758090cb0e8a306d48
Fredrik Johansson <jeanfredrik@gmail.com>
2024-10-23 12:36:20 +0200

    Adjust styling of timeline

--- psr-4/ComponentLibrary/Component/Timeline/timeline.blade.php
+++ psr-4/ComponentLibrary/Component/Timeline/timeline.blade.php
@@ -1,6 +1,6 @@
 <!-- timeline.blade.php -->
 <ul
-  class="relative py-4 before:absolute before:left-4 before:top-0 before:bottom-0 before:border-l before:border-2 before:border-layer before:ml-[-1px] space-y-3 text-black">
+  class="[--timeline-indent:12px] relative py-4 before:absolute before:left-[--timeline-indent] before:top-0 before:bottom-0 before:border-l-2 before:border-r-0 before:border-y-0 before:border-l-layer before:ml-[-1px] space-y-3 text-black">
   @php $latestEvent = ''; @endphp
   @foreach ($events as $event)
     @php
@@ -14,10 +14,10 @@
       $hasPassed = (bool) (strtotime($event['date']) < strtotime('0:00'));
       $currentEvent = $latestEvent == strtotime($event['date']);
     @endphp
-    <li class="grid grid-cols-[max-content_1fr] gap-6 items-center">
+    <li class="grid grid-cols-[calc(var(--timeline-indent)*2)_1fr] gap-5 items-center">
       <div
         class="{{ clsx([
-            'rounded-full size-8 border-[3px] border-layer flex place-items-center place-content-center text-[1.5rem]',
+            'rounded-full size-8 border-[3px] border-layer flex place-items-center place-content-center text-[1.5rem] justify-self-center',
             'bg-layer text-dark' => $hasPassed && !$currentEvent,
             'bg-success-dark text-white' => $currentEvent,
             'bg-[--timeline-bg]' => !$currentEvent && !$hasPassed,
@@ -31,7 +31,7 @@
         @endif
       </div>
       <div
-        class="relative after:absolute after:border-r-layer after:left-0 after:top-1/2 after:border-y-[1.5em] after:border-r-[2em] after:ml-[-2em] after:border-transparent after:mt-[-1.5em] after:text-[length:.5rem]">
+        class="relative after:absolute after:border-r-layer after:left-0 after:top-1/2 after:border-y-[.75em] after:border-r-[1em] after:ml-[-1em] after:border-transparent after:mt-[-.75em] after:text-[length:10px] [--color-background-card:var(--color-layer)]">
         @component('mxui.card', [
             'link' => $event['link'],
             'meta' => $event['timelineDate'],
