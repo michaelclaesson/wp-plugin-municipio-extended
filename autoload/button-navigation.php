a731a4fc853f37bb25a8ed4a26f95aa4721a71ca
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-10-24 10:34:57 +0200

    fix: adjust button for previous page to appear before children page buttons

--- autoload/button-navigation.php
+++ autoload/button-navigation.php
@@ -1,5 +1,10 @@
 <?php
 
+add_action("article_content_before", function () {
+  $post = mx_get_post();
+  echo mx_render_view("section-back-button", ["post" => $post]);
+});
+
 add_filter(
   "Municipio/Customizer/Sections/General/secondary_navigation_position",
   function ($options) {
