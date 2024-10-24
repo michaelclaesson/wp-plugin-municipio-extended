4568feb2c850c040c5c0bd186ccd636e9f5a89be
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-10-24 14:08:15 +0200

    feat: initial styles

---
+++ views/mxui/segment/card.blade.php
@@ -0,0 +1,54 @@
+{{--
+containerAware
+dateBadge
+height
+icon
+layout
+overlay
+reverseColumns
+sub_title
+
+buttons
+--}}
+
+<!-- mxui.segment.card -->
+<div class="@container">
+  <section
+    {{ mx_attrs(
+        [
+            'class' => ['flex flex-col min-h-full group @[50rem]:flex-row @[50rem]:max-w-[1920px]', $classList],
+        ],
+        $attributeList,
+    ) }}>
+    <div class="-mt-8 mx-4 w-auto @[50rem]:mt-8 @[50rem]:absolute @[50rem]:w-1/2 @[50rem]:left-1/2 @[50rem]:mx-0">
+      @component('mxui.card', [
+          'heading' => $title,
+          'content' => new \Illuminate\Support\HtmlString($content),
+          'date' => $date,
+          'meta' => $meta,
+          'tags' => $tags,
+          'link' => $link,
+          'wrapContent' => true,
+          'classList' => ['[--card-px:2.5rem] [--card-py:1.5rem] @[50rem]:min-h-80 isolate'],
+          'expandLinkCover' => true,
+      ])
+      @endcomponent
+    </div>
+    <div
+      {{ mx_attrs([
+          'class' => [
+              'w-full @[50rem]:relative @[50rem]:w-3/4 @[50rem]:max-h-96',
+              '-order-1',
+              'aspect-video bg-secondary first:last:mb-0',
+              'overflow-hidden border-none rounded-[var(--c-segment-image-border-radius,var(--radius-lg,calc(var(--base,8px)*1.5)))]',
+          ],
+      ]) }}>
+      @component('mxui.image', [
+          'image' => $image,
+          'size' => 'medium',
+          'classList' => 'group-hover:scale-105 transition-transform duration-500 text-transparent',
+      ])
+      @endcomponent
+    </div>
+  </section>
+</div>
