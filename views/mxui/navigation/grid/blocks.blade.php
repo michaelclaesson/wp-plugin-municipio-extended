609e555976d36a7e681a3883515e5484d6e9f866
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-10-29 07:54:41 +0100

    fix: add support to tab target mxui navigation grid blocks

--- views/mxui/navigation/grid/blocks.blade.php
+++ views/mxui/navigation/grid/blocks.blade.php
@@ -1,7 +1,7 @@
 <ul class="grid grid-cols-[repeat(auto-fill,minmax(min(100%,18rem),1fr))] gap-2 sm:gap-6">
   @foreach($items as $item)
-    <li class="relative p-4 bg-complementary-light hover:bg-primary text-base group">
-      <a href="{{ $item['href'] }}" class="group after:absolute after:inset-0 after:z-[1] after:inert:hidden">
+    <li class="relative bg-complementary-light hover:bg-primary text-base group">
+      <a href="{{ $item['href'] }}" class="block w-full h-full group-hover:outline-none p-4">
         <div class="space-y-2">
           <div class="typography-h3 leading-none underline group-hover:text-primary-contrasting">
             {{ $item['title'] }}
