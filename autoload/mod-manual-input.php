2d513485fcddc4487200331619fa7ea691d73487
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-11-01 12:06:01 +0100

    fix: restore the link_text field for manual input segment

--- autoload/mod-manual-input.php
+++ autoload/mod-manual-input.php
@@ -4,14 +4,3 @@ add_filter("Modularity/Module/ManualInput/DefaultValues", function ($values) {
   $values["link_text"] = "";
   return $values;
 });
-
-// Remove text field for segment
-add_action(
-  "admin_init",
-  function () {
-    if (function_exists("acf_remove_local_field")) {
-      acf_remove_local_field("field_65002bce6d459");
-    }
-  },
-  20,
-);
