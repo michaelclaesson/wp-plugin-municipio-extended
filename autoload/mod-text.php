9b278790756e4a43e8afaaefe596f8d5c1d485a8
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-11-08 10:41:22 +0100

    fix:format files

--- autoload/mod-text.php
+++ autoload/mod-text.php
@@ -1,61 +1,60 @@
 <?php
 
-    add_filter("Modularity/Display/mod-text/viewData", function($data) {
-    if (!empty($data['post_content'])) {
-        $data['post_content'] = replaceBuiltinClasses($data['post_content']);
-    }
-    return $data;
+add_filter("Modularity/Display/mod-text/viewData", function ($data) {
+  if (!empty($data["post_content"])) {
+    $data["post_content"] = mx_replace_builtin_classes($data["post_content"]);
+  }
+  return $data;
 });
 
-    function replaceBuiltinClasses($content)
-  {
-      return str_replace(
-          [
-              'wp-caption',
-              'c-image-text',
-              'wp-image-',
-              'alignleft',
-              'alignright',
-              'alignnone',
-              'aligncenter',
+function mx_replace_builtin_classes($content) {
+  return str_replace(
+    [
+      "wp-caption",
+      "c-image-text",
+      "wp-image-",
+      "alignleft",
+      "alignright",
+      "alignnone",
+      "aligncenter",
 
-              //Old inline transition button
-              'btn-theme-first',
-              'btn-theme-second',
-              'btn-theme-third',
-              'btn-theme-fourth',
-              'btn-theme-fifth',
+      //Old inline transition button
+      "btn-theme-first",
+      "btn-theme-second",
+      "btn-theme-third",
+      "btn-theme-fourth",
+      "btn-theme-fifth",
 
-              //Gutenberg block image
-              'wp-block-image',
-              'wp-element-caption',
-              '<figcaption>'
-          ],
-          [
-              'c-image',
-              'c-image__caption',
-              'c-image__image wp-image-',
-              'u-float--left@sm u-float--left@md u-float--left@lg u-float--left@xl u-float--left@xl u-margin__y--2 
+      //Gutenberg block image
+      "wp-block-image",
+      "wp-element-caption",
+      "<figcaption>",
+    ],
+    [
+      "c-image",
+      "c-image__caption",
+      "c-image__image wp-image-",
+      'u-float--left@sm u-float--left@md u-float--left@lg u-float--left@xl u-float--left@xl u-margin__y--2 
               u-margin__right--2@sm u-margin__right--2@md u-margin__right--2@lg u-margin__right--2@xl 
               u-width--100@xs',
-              'u-float--right@sm u-float--right@md u-float--right@lg u-float--right@xl u-float--right@xl 
+      'u-float--right@sm u-float--right@md u-float--right@lg u-float--right@xl u-float--right@xl 
               u-margin__y--2 u-margin__left--2@sm u-margin__left--2@md u-margin__left--2@lg u-margin__left--2@xl 
               u-width--100@xs',
-              '',
-              'u-margin__x--auto u-text-align--center',
+      "",
+      "u-margin__x--auto u-text-align--center",
 
-              //Old inline transition button
-              'c-button c-button__filled c-button__filled--primary c-button--md',
-              'c-button c-button__filled c-button__filled--secondary c-button--md',
-              'c-button c-button__filled c-button__filled--secondary c-button--md',
-              'c-button c-button__filled c-button__filled--secondary c-button--md',
-              'c-button c-button__filled c-button__filled--secondary c-button--md',
+      //Old inline transition button
+      "c-button c-button__filled c-button__filled--primary c-button--md",
+      "c-button c-button__filled c-button__filled--secondary c-button--md",
+      "c-button c-button__filled c-button__filled--secondary c-button--md",
+      "c-button c-button__filled c-button__filled--secondary c-button--md",
+      "c-button c-button__filled c-button__filled--secondary c-button--md",
 
-              //Gutenberg block image
-              'c-image',
-              'c-image__caption',
-              '<figcaption class="c-image__caption">'
-          ],
-          $content
-      );
-  }
+      //Gutenberg block image
+      "c-image",
+      "c-image__caption",
+      '<figcaption class="c-image__caption">',
+    ],
+    $content,
+  );
+}
