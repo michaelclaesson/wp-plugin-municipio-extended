c46c501ed0170ead26ae9606d8366bb2fb4e97ee
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-11-12 14:59:12 +0100

    fix: add filter to fix styles

--- autoload/mod-manual-input.php
+++ autoload/mod-manual-input.php
@@ -4,3 +4,16 @@ add_filter("Modularity/Module/ManualInput/DefaultValues", function ($values) {
   $values["link_text"] = "";
   return $values;
 });
+
+add_filter("Modularity/Display/mod-manualinput/viewData", function ($data) {
+  if (!empty($data["manualInputs"]) && is_array($data["manualInputs"])) {
+    foreach ($data["manualInputs"] as $index => $input) {
+      if (!empty($input["content"])) {
+        $data["manualInputs"][$index]["content"] = mx_replace_builtin_classes(
+          $input["content"],
+        );
+      }
+    }
+  }
+  return $data;
+});
