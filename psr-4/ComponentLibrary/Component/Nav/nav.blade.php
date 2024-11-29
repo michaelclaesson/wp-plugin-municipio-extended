c79f6d51d07ebffd48311ce60454d03ddeef870b
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2024-11-29 20:40:45 +0100

    fix: Hyphenate long words in menus and puffs

--- psr-4/ComponentLibrary/Component/Nav/nav.blade.php
+++ psr-4/ComponentLibrary/Component/Nav/nav.blade.php
@@ -6,7 +6,7 @@
         <li id="{{ $id }}-{{ $item['id'] }}-{{ $loop->index }}__item"
           class="{{ $itemClass($item, $direction) }}" {!! $buildAttributes($item['attributeList']) !!}>
 
-          <div class="{{ $baseClass }}__item-wrapper">
+          <div class="{{ $baseClass }}__item-wrapper nav-link">
             {{-- Nav item --}}
             @if ($allowStyle)
               @includeIf('Nav.style.' . $item['style'])
