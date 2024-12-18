2f85577ae72cd8898f92e95dea65683796d6f0c1
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2024-12-18 14:49:18 +0100

    wip: taxonomies for postmodule card

--- autoload/mod-posts.php
+++ autoload/mod-posts.php
@@ -42,3 +42,55 @@ add_filter("Modularity/Display/mod-posts/viewData", function ($data) {
   $data["lang"]["readMore"] = "";
   return $data;
 });
+
+// Add taxonomies field to cards and list
+add_filter("acf/load_field/key=field_571e01e7f246c", function ($field) {
+  $field["conditional_logic"] = [
+    [
+      [
+        "field" => "field_571dfd4c0d9d9",
+        "operator" => "==",
+        "value" => "mixed",
+      ],
+    ],
+  ];
+
+  $field["choices"]["taxonomies"] = __("Visa taxonomier", "municipio-extended");
+
+  return $field;
+});
+
+// Add sub-field for taxonomy selection under "Fält"
+add_action("acf/init", function () {
+  acf_add_local_field([
+    "key" => "field_taxonomy_selection_in_fields",
+    "label" => __("Taxonomier att visa för kort", "municipio-extended"),
+    "name" => "taxonomy_selection_in_fields",
+    "type" => "checkbox",
+    "parent" => "group_571dfd3c07a77",
+    "conditional_logic" => [
+      [
+        [
+          "field" => "field_571e01e7f246c",
+          "operator" => "==",
+          "value" => "taxonomies",
+        ],
+      ],
+    ],
+    "choices" => [],
+    "layout" => "horizontal",
+  ]);
+});
+
+// Populate taxonomy choices
+add_filter("acf/load_field/key=field_taxonomy_selection_in_fields", function (
+  $field,
+) {
+  $taxonomies = get_taxonomies(["public" => true], "objects");
+
+  foreach ($taxonomies as $taxonomy) {
+    $field["choices"][$taxonomy->name] = $taxonomy->label;
+  }
+
+  return $field;
+});
