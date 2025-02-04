791df98ebcbc50872bb843a229bf132250b156de
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2025-02-04 18:57:12 +0100

    feat: add new widget area related pages

---
+++ autoload/custom-widgets.php
@@ -0,0 +1,16 @@
+<?php
+function register_custom_widget_area() {
+  register_sidebar([
+    "id" => "related-pages-area",
+    "name" => __("Related pages sidebar", "municipio-extended"),
+    "description" => __(
+      "Widget area made for related pages",
+      "municipio-extended",
+    ),
+    "before_title" => '<h2 class="c-typography c-typography__variant--h3">',
+    "after_title" => "</h2>",
+    "before_widget" => '<div id="%1$s" class="%2$s">',
+    "after_widget" => "</div>",
+  ]);
+}
+add_action("widgets_init", "register_custom_widget_area");
