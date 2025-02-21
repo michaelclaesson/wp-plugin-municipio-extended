ebcccab3f430445191d44b92aeb917c0db6ca6a8
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2025-02-21 14:18:39 +0100

    feat: add under-hero widget area

--- autoload/custom-widgets.php
+++ autoload/custom-widgets.php
@@ -12,5 +12,17 @@ function register_custom_widget_area() {
     "before_widget" => '<div id="%1$s" class="%2$s">',
     "after_widget" => "</div>",
   ]);
+  register_sidebar([
+    "id" => "under-hero",
+    "name" => __("Under hero sidebar", "municipio-extended"),
+    "description" => __(
+      "Widget area made for under hero",
+      "municipio-extended",
+    ),
+    "before_title" => '<h2 class="c-typography c-typography__variant--h3">',
+    "after_title" => "</h2>",
+    "before_widget" => '<div id="%1$s" class="%2$s">',
+    "after_widget" => "</div>",
+  ]);
 }
 add_action("widgets_init", "register_custom_widget_area");
