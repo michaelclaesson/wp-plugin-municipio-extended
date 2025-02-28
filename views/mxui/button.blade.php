03c809a4a50700728e5f0027408c6004148626c9
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2025-02-28 15:07:00 +0100

    fix: change padding-y

--- views/mxui/button.blade.php
+++ views/mxui/button.blade.php
@@ -1,7 +1,7 @@
 @include('mxui.clickable', [
     'classList' => [
         'rounded-[var(--c-button-border-radius,var(--radius-md,var(--base,8px)))]',
-        'text-[.95rem] min-h-12 min-w-18 py-3 px-5',
+        'text-[.95rem] min-h-10 min-w-18 py-2 px-5',
         'inline-flex gap-1 items-center',
         'transition-colors',
         'font-[number:var(--button-font-weight,var(--font-weight-button,500))]',
