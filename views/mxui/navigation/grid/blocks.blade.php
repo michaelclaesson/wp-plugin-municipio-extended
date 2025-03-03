efcb83b8d8aee82cfdd4550c5a2c443871623a33
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2025-03-03 15:02:24 +0100

    feat: add border-radius variable to nav grid blocks

--- views/mxui/navigation/grid/blocks.blade.php
+++ views/mxui/navigation/grid/blocks.blade.php
@@ -1,7 +1,7 @@
 <ul class="grid grid-cols-[repeat(auto-fill,minmax(min(100%,18rem),1fr))] gap-2 sm:gap-6">
   @foreach($items as $item)
     <li>
-      <a href="{{ $item['href'] }}" class="block w-full h-full p-4 bg-complementary-light hover:bg-primary hover:text-primary-contrasting transition-none">
+      <a href="{{ $item['href'] }}" class="block w-full h-full p-4 bg-complementary-light hover:bg-primary hover:text-primary-contrasting transition-none" style="border-radius: var(--radius-lg);">
         <div class="space-y-2">
           <div class="typography-h3 leading-none underline">
             {{ $item['title'] }}
