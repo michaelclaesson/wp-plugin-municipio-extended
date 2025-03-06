f6af94d874a116bdcf464044addbc3e3c36cbb21
Ruslana Zholobnytska <63247495+RuslanaLogosha@users.noreply.github.com>
2025-03-06 21:00:32 +0100

    fix: delete logs

--- autoload/mod-video.php
+++ autoload/mod-video.php
@@ -12,22 +12,18 @@ add_filter("acf/load_field/key=field_57454c7ad44dc", function ($field) {
 
 add_filter("Modularity/Display/mod-video/viewData", function ($data) {
   if (!empty($data)) {
-    // var_dump($data["embed_link"]);
-
-    error_log("data: " . print_r($data["embed_link"], true));
     $embedLink = $data["embed_link"];
     if (
       is_string($embedLink) &&
       preg_match("/\bplay\.mediaflowpro\b/", $embedLink)
     ) {
-      // Extract the matched part of the string using a regular expression
       preg_match(
         '/"([^"]+\bplay\.mediaflowpro\b[^"]+)"/',
         $embedLink,
         $matches,
       );
       if (isset($matches[1])) {
-        $data["embed_link"] = $matches[1]; // Assign the matched URL
+        $data["embed_link"] = $matches[1];
       }
     }
   }
