93fb71085da7210553cf3ec013af3171bc4e6fcf
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-07 14:13:19 +0100

    Use Modularity/Display/mod-video/pre_getEmbedMarkup filter

--- autoload/mod-video.php
+++ autoload/mod-video.php
@@ -29,3 +29,17 @@ add_filter("Modularity/Display/mod-video/viewData", function ($data) {
   }
   return $data;
 });
+
+add_filter("Modularity/Display/mod-video/pre_getEmbedMarkup", function (
+  $markup,
+  $embedLink,
+) {
+  if (
+    is_string($embedLink) &&
+    preg_match("/\bplay\.mediaflowpro\b/", $embedLink)
+  ) {
+    // For videos from mediaflow
+    return $embedLink;
+  }
+  return $markup;
+});
