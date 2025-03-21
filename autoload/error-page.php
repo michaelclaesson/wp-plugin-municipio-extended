1665ca03eed305a0e65b4de7baee0b80264e4068
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-21 16:08:32 +0100

    Potentially solve issue with 404 routing

--- autoload/error-page.php
+++ autoload/error-page.php
@@ -27,10 +27,18 @@ add_action(
         $original_is_404 = $wp_query->is_404;
         $original_is_page = $wp_query->is_page;
         $original_is_singular = $wp_query->is_singular;
+        $original_queried_object_id = $wp_query->queried_object_id;
+        $original_post_count = $wp_query->post_count;
+        $original_current_post = $wp_query->current_post;
+        $original_posts = $wp_query->posts;
 
         $wp_query->is_404 = false;
         $wp_query->is_page = true;
         $wp_query->is_singular = true;
+        $wp_query->queried_object_id = $post->ID;
+        $wp_query->post_count = 1;
+        $wp_query->current_post = -1;
+        $wp_query->posts = [$post];
         $post = $custom_404_page;
 
         setup_postdata($post);
@@ -46,6 +54,12 @@ add_action(
         $wp_query->is_404 = $original_is_404;
         $wp_query->is_page = $original_is_page;
         $wp_query->is_singular = $original_is_singular;
+        $wp_query->queried_object_id = $original_queried_object_id;
+        $wp_query->post_count = $original_post_count;
+        $wp_query->current_post = $original_current_post;
+        $wp_query->posts = $original_posts;
+
+        setup_postdata($original_post);
       }
     }
   },
