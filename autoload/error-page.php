3076ada40fb6f15f2ac7451c6b949225bbcf6d58
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-21 17:27:44 +0100

    Potentially solve issue with 404 routing

--- autoload/error-page.php
+++ autoload/error-page.php
@@ -2,10 +2,10 @@
 
 function mx_get_custom_404_page() {
   static $custom_page;
-  if ($custom_page) {
+  if (isset($custom_page)) {
     return $custom_page;
   }
-  $custom_page = get_page_by_path("page-not-found");
+  $custom_page = get_page_by_path("page-not-found") ?: false;
   return $custom_page;
 }
 
@@ -15,7 +15,7 @@ function is_custom_404() {
 }
 
 add_action(
-  "template_redirect",
+  "wp",
   function () {
     if (is_404()) {
       $custom_404_page = mx_get_custom_404_page();
@@ -23,16 +23,8 @@ add_action(
         // Reset query flags so that WordPress treats this as a regular page.
         global $wp_query, $post;
 
-        $original_post = $post;
-        $original_is_404 = $wp_query->is_404;
-        $original_is_page = $wp_query->is_page;
-        $original_is_singular = $wp_query->is_singular;
-        $original_queried_object_id = $wp_query->queried_object_id;
-        $original_post_count = $wp_query->post_count;
-        $original_current_post = $wp_query->current_post;
-        $original_posts = $wp_query->posts;
-
         $wp_query->is_404 = false;
+        $wp_query->is_front_page = false;
         $wp_query->is_page = true;
         $wp_query->is_singular = true;
         $wp_query->queried_object_id = $post->ID;
@@ -42,28 +34,10 @@ add_action(
         $post = $custom_404_page;
 
         setup_postdata($post);
-
-        $file = get_page_template() ?: get_query_template("page");
-
-        if (file_exists($file)) {
-          include $file;
-          exit();
-        }
-
-        $post = $original_post;
-        $wp_query->is_404 = $original_is_404;
-        $wp_query->is_page = $original_is_page;
-        $wp_query->is_singular = $original_is_singular;
-        $wp_query->queried_object_id = $original_queried_object_id;
-        $wp_query->post_count = $original_post_count;
-        $wp_query->current_post = $original_current_post;
-        $wp_query->posts = $original_posts;
-
-        setup_postdata($original_post);
       }
     }
   },
-  9,
+  10,
 );
 
 add_action(
