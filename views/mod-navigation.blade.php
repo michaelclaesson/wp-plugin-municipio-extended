16ca1ea7ab14d9409a6a0311755fd699a6fd2bc9
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-21 09:25:24 +0100

    Don't render tailwind tag if there is no navigation

--- views/mod-navigation.blade.php
+++ views/mod-navigation.blade.php
@@ -10,34 +10,32 @@
   @endtypography
 @endif
 
-<div class="tailwind">
-  @switch($format)
-    @case ('list')
-      @include('mxui.navigation.list')
-    @break
+@switch($format)
+  @case ('list')
+    @include('mxui.navigation.list')
+  @break
 
-    @case ('grid')
-      @include('mxui.navigation.grid')
-    @break
+  @case ('grid')
+    @include('mxui.navigation.grid')
+  @break
 
-    @case ('bar')
-      @include('mxui.navigation.bar')
-    @break
+  @case ('bar')
+    @include('mxui.navigation.bar')
+  @break
 
-    @case ('tree')
-      @include('mxui.navigation.tree')
-    @break
+  @case ('tree')
+    @include('mxui.navigation.tree')
+  @break
 
-    @case ('cards')
-      @include('mxui.navigation.cards')
-    @break
+  @case ('cards')
+    @include('mxui.navigation.cards')
+  @break
 
-    @case ('buttons')
-      @include('mxui.navigation.buttons')
-    @break
+  @case ('buttons')
+    @include('mxui.navigation.buttons')
+  @break
 
-    @case ('inline')
-      @include('mxui.navigation.inline')
-    @break
-  @endswitch
-</div>
+  @case ('inline')
+    @include('mxui.navigation.inline')
+  @break
+@endswitch
