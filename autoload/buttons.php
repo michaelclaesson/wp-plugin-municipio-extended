5fe2b685c2b0dce3a4fd798632b456e4b6a1f9f2
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-23 22:17:57 +0100

    Fix button styles and add outlined variant

---
+++ autoload/buttons.php
@@ -0,0 +1,25 @@
+<?php
+
+function mx_content_to_prose($content, $options = []) {
+  $content = preg_replace_callback(
+    '/(<a\s[^>]*class="((?:[^"]+\s)c-button)([^"]*)"\s[^>]*href="([^"]+)"[^>]*>(.*?)<\/a>)/',
+    function ($matches) {
+      $classes = preg_split("/\s+/", $matches[2]);
+      $href = $matches[4];
+      $label = strip_tags($matches[5]);
+      return mx_render_view("mxui.button", [
+        "href" => $href,
+        "label" => $label,
+        "variant" => in_array("c-button__outlined", $classes)
+          ? "outlined"
+          : (in_array("c-button__filled--primary", $classes)
+            ? "primary"
+            : (in_array("c-button__filled--secondary", $classes)
+              ? "secondary"
+              : "default")),
+      ]);
+    },
+    $content,
+  );
+  return $content;
+}
--- views/mxui/button.blade.php
+++ views/mxui/button.blade.php
@@ -4,12 +4,17 @@
         'text-[.95rem] min-h-10 min-w-18 py-2 px-5',
         'inline-flex gap-1 items-center',
         'transition-colors',
+        'no-underline border-[length:var(--border-width-button,var(--border-width-medium,2px))] border',
         'font-[number:var(--button-font-weight,var(--font-weight-button,500))]',
         [
-            'primary' => 'bg-primary hover:bg-primary-dark text-primary-contrasting hover:text-primary-contrasting',
+            'primary' =>
+                'bg-primary hover:bg-primary-dark text-primary-contrasting visited:text-primary-contrasting hover:text-primary-contrasting hover:visited:text-primary-contrasting border-transparent',
             'secondary' =>
-                'bg-secondary hover:bg-primary text-secondary-contrasting hover:text-primary-contrasting',
-        ][$variant ?? 'default'] ?? 'bg-layer hover:bg-primary hover:text-primary-contrasting',
+                'bg-secondary hover:bg-primary text-secondary-contrasting visited:text-secondary-contrasting hover:text-primary-contrasting hover:visited:text-primary-contrasting border-transparent',
+            'outlined' =>
+                'bg-transparent hover:bg-[#00000018] text-primary visited:text-primary border-primary hover:text-primary-dark hover:visited:text-primary-dark hover:border-primary-dark',
+        ][$variant ?? 'default'] ??
+        'bg-layer hover:bg-primary hover:text-primary-contrasting hover:visited:text-primary-contrasting border-transparent',
         $classList ?? null,
     ],
 ])
