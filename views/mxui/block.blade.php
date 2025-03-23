371ca6ac5635e044ba0a7ff6625effc280903b77
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-23 22:20:33 +0100

    Fix: Styling of event heroes

--- views/mxui/block.blade.php
+++ views/mxui/block.blade.php
@@ -8,46 +8,50 @@ background
 @endphp
 
 <!-- mxui.block -->
-<section {{ mx_attrs([
-    'class' => ['group grid', $classList],
-    $attributeList,
+<div {{ mx_attrs([
+    'class' => ['tailwind', $classList],
 ]) }}>
-  @if ($title || $content)
+  <section {{ mx_attrs([
+      'class' => ['group grid'],
+      $attributeList,
+  ]) }}>
+    @if ($title || $content)
+      <div
+        {{ mx_attrs([
+            'class' => [
+                'relative isolate row-start-1 col-start-1 z-2 text-white',
+                'bg-[color:var(--c-segment-color-overlay,var(--color-alpha,#0000008c))]' => !empty($overlay),
+                'flex flex-col text-center items-center justify-center',
+            ],
+        ]) }}>
+        <div class="prose">
+          @if ($title)
+            <h1>
+              {{ $title }}
+            </h1>
+          @endif
+          @if ($content)
+            <p>{{ $content }}</p>
+          @endif
+        </div>
+      </div>
+    @endif
     <div
       {{ mx_attrs([
           'class' => [
-              'relative isolate row-start-1 col-start-1 z-2 text-white',
-              'bg-[color:var(--c-segment-color-overlay,var(--color-alpha,#0000008c))]' => !empty($overlay),
-              'flex flex-col text-center items-center justify-center',
+              'row-start-1 col-start-1 w-full',
+              'aspect-video bg-secondary first:last:mb-0',
+              'overflow-hidden border-none',
           ],
       ]) }}>
-      <div class="prose">
-        @if ($title)
-          <h1>
-            {{ $title }}
-          </h1>
-        @endif
-        @if ($content)
-          <p>{{ $content }}</p>
-        @endif
-      </div>
+      @if ($image ?? null)
+        @component('mxui.image', [
+            'image' => $image,
+            'size' => 'medium',
+            'classList' => ['text-transparent', 'group-hover:scale-105 transition-transform duration-500' => $link],
+        ])
+        @endcomponent
+      @endif
     </div>
-  @endif
-  <div
-    {{ mx_attrs([
-        'class' => [
-            'row-start-1 col-start-1 w-full',
-            'aspect-video bg-secondary first:last:mb-0',
-            'overflow-hidden border-none',
-        ],
-    ]) }}>
-    @if ($image ?? null)
-      @component('mxui.image', [
-          'image' => $image,
-          'size' => 'medium',
-          'classList' => ['text-transparent', 'group-hover:scale-105 transition-transform duration-500' => $link],
-      ])
-      @endcomponent
-    @endif
-  </div>
-</section>
+  </section>
+</div>
