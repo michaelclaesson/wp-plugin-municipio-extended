e6ac92f8c5e13212c6624b52529c05b265d1b9b0
Fredrik Johansson <jeanfredrik@gmail.com>
2025-04-05 20:04:27 +0200

    Add debug page instead of logging fonts

--- autoload/fonts.php
+++ autoload/fonts.php
@@ -30,7 +30,7 @@ function mx_get_uploaded_fonts() {
   // Cache the fonts for 1 minute (60 seconds)
   set_transient("mx_uploaded_fonts", $fonts, 60);
 
-  mx_error_log("new mx_uploaded_fonts transient value", $fonts);
+  // mx_error_log("new mx_uploaded_fonts transient value", $fonts);
 
   return $fonts;
 }
