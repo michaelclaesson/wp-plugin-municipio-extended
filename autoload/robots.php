b88c17d710a43f5d85367a7608c75e416c35e850
Fredrik Johansson <jeanfredrik@gmail.com>
2025-06-03 11:23:05 +0200

    Disallow robots indexing of uploads

--- autoload/robots.php
+++ autoload/robots.php
@@ -3,9 +3,24 @@
 add_filter(
   "robots_txt",
   function ($output, $public) {
-    if ($public) {
-      $site_url = get_site_url();
-      $output .= "Sitemap: {$site_url}/sitemap.xml\n";
+    $output = "";
+    $output .= "User-agent: *\n";
+    $output .= "Disallow: /wp-admin/\n";
+    $output .= "Allow: /wp-admin/admin-ajax.php\n";
+
+    $upload_dir = wp_get_upload_dir();
+    if ($upload_dir["baseurl"] ?? null) {
+      $upload_url = str_replace(home_url(), "", $upload_dir["baseurl"]);
+      $output .= "Disallow: {$upload_url}/\n";
+    }
+
+    $site_url = get_site_url();
+    $output .= "\n";
+    $output .= "Sitemap: {$site_url}/sitemap.xml\n";
+
+    if (!$public) {
+      $output .= "\n";
+      $output .= "Disallow: /\n";
     }
     return $output;
   },
