a2447729b2e6f4895006d40fcebf7ce94c1c9895
jeanfredrik <jeanfredrik@gmail.com>
2025-10-11 15:38:46 +0200

    Add ability to customize quote background color

--- autoload/typography.php
+++ autoload/typography.php
@@ -4,6 +4,7 @@ use Kirki\Compatibility\Kirki;
 
 add_action("init", function () {
   $section_id = "municipio_customizer_section_typography";
+
   Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
     "type" => "color",
     "settings" => "page_title_color",
@@ -19,6 +20,25 @@ add_action("init", function () {
       [
         "element" => ":root",
         "property" => "--page-title-color",
+      ],
+    ],
+  ]);
+
+  Kirki::add_field(\Municipio\Customizer::KIRKI_CONFIG, [
+    "type" => "color",
+    "settings" => "quote_background_color",
+    "label" => __("Quote background color", "municipio-extended"),
+    "description" => __(
+      "If no color is selected, the light secondary color will be used.",
+      "municipio-extended",
+    ),
+    "section" => $section_id,
+    "priority" => 100,
+    "default" => null,
+    "output" => [
+      [
+        "element" => ":root",
+        "property" => "--quote-background-color",
       ],
     ],
   ]);
