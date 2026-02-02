ae0c17abe2f563d4880711a0d6f69e4edd0e701f
jeanfredrik <jeanfredrik@gmail.com>
2026-02-02 22:51:33 +0000

    Publish version 2025.12.4

--- municipio-extended.php
+++ municipio-extended.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Municipio Extended
  * Description: Adds more features to Municipio.
- * Version: 2025.12.3
+ * Version: 2025.12.4
  * Author: Whitespace Dev
  * Text Domain: municipio-extended
  * Domain Path: /languages/
